133b92c5627fea3fd77aaca505efb75e1b512b16
Ian Harvey <ith.harvey@gmail.com>
2017-06-07 14:11:02 -0700

    progress on the actions component

--- client-service/app/public/main-page/actions.component.js
+++ client-service/app/public/main-page/actions.component.js
@@ -10,9 +10,26 @@
     const vm = this
     vm.actions = []
 
-  vm.$onInit = function () {
-    vm.actions = actionsService.getActions()
+    vm.$onInit = function () {
+      console.log(vm.actions)
+      actionsService.getActions().then( response => {
+        console.log(response.data);
+        vm.actions = response.data
+      })
+    }
 
+    vm.updateAction = function (action,keyUpdating,id) {
+      console.log('this is the key', action[keyUpdating]);
+
+      if(keyUpdating === 'complete') {
+        action[keyUpdating] = true
+      } else {
+        action[keyUpdating] = false
+      }
+
+      actionsService.updateAction(keyUpdating,id).then( response => {
+        console.log('this is the response', response);
+      })
     }
 
   }
--- client-service/app/public/services/dashboard.actions.Service.js
+++ client-service/app/public/services/dashboard.actions.Service.js
@@ -15,15 +15,22 @@
     //   const userLogout = `http://localhost:5000/api/auth/logout`
     //   return $http.delete(userLogout)
     // }
-    //
-    // vm.register = function(newUser){
-    //   const userUrl = `http://localhost:8080/users`
-    //   return $http.post(userUrl, newUser)
-    // }
 
+    vm.updateAction = function(keyUpdating,id) {
+      const actionsUrl = 'https://job-tracker-apps-service.herokuapp.com/actions/' + id
+      let bool
+      if (keyUpdating === 'complete') {
+        bool = true
+      } else {
+        bool = false
+      }
+      const data = `{"${keyUpdating}": ${bool}}`
+      console.log('data', data);
+      return $http.patch(actionsUrl,data)
+    }
 
     vm.getActions = function() {
-      const actionsUrl = 'http://localhost:8080/actions'
+      const actionsUrl = 'https://job-tracker-apps-service.herokuapp.com/actions'
       return $http.get(actionsUrl)
     }
 
