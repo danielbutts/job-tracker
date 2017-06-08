b5d3b2adc24085f3f7afabb496a2d781c1e9c9e8
danielbutts <daniel.butts@gmail.com>
2017-06-08 16:18:03 -0700

    completed new application form

--- client-service/app/public/app.config.js
+++ client-service/app/public/app.config.js
@@ -3,7 +3,8 @@
 
   angular.module('app')
   .constant('APPLICATION_SERVICE_URL', 'https://job-tracker-apps-service.herokuapp.com')
-  .constant('JOB_SERVICE_URL', 'https://job-tracker-job-service.herokuapp.com')
+  // .constant('JOB_SERVICE_URL', 'https://job-tracker-job-service.herokuapp.com')
+  .constant('JOB_SERVICE_URL', 'http://localhost:8081')
   .constant('USER_SERVICE_URL', 'https://job-tracker-user-service.herokuapp.com')
   .config(config)
 
