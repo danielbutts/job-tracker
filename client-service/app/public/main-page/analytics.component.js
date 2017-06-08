bbef69012609953000c921c2eb4cffafe4daab90
Mike <mahathaway93@gmail.com>
2017-06-08 11:07:44 -0700

    added basic line graph

--- client-service/app/public/main-page/analytics.component.js
+++ client-service/app/public/main-page/analytics.component.js
@@ -11,17 +11,36 @@
 
     vm.$onInit = function(){
 
-      console.log($cookies.get('id'))
-
       analayticsService.getApplicationData()
         .then((response) => {
-          console.log('data from analytics service!',response[1])
-          //set the chart.js variables in here
-            // vm.date = ; vm.labels =
+          console.log('data from analytics service!',response)
 
-            //vm.date = Object.keys(response[1]).filter(application => application.)
+          // vm.series ['Average', 'SingleUser']
+          // vm.labels = ['All Time']
+          // vm.data = [[response.averageAppsAllUsers],[response.userApplications.length]]
 
-          vm.applications = response.data
+          vm.labels = ["January", "February", "March", "April", "May", "June", "July"]
+          vm.data = [[65, 59, 80, 81, 56, 55, 40], [28, 48, 40, 19, 86, 27, 90]]
+          vm.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }]
+          vm.options = {
+            scales: {
+              yAxes: [
+                {
+                  id: 'y-axis-1',
+                  type: 'linear',
+                  display: true,
+                  position: 'left'
+                },
+                {
+                  id: 'y-axis-2',
+                  type: 'linear',
+                  display: true,
+                  position: 'right'
+                }
+              ]
+            }
+          }
+          //vm.date = Object.keys(response[1]).filter(application => application.)
         })
         .catch((err) => console.error(err))
     }
--- client-service/app/public/services/analyticsService.js
+++ client-service/app/public/services/analyticsService.js
@@ -2,18 +2,26 @@
 
   angular.module('app').service('analayticsService', analayticsService)
 
-  analayticsService.$inject = ['$http']
-  function analayticsService($http){
+  analayticsService.$inject = ['$http', '$cookies']
+  function analayticsService($http, $cookies){
     const vm = this
 
+    // responsible for wrangling data into useable format
     vm.getApplicationData = function(){
 
+      const userId = $cookies.get('id')
       const allUsersURL = 'https://job-tracker-user-service.herokuapp.com/users'
       const allApplicationsURL = 'https://job-tracker-apps-service.herokuapp.com/applications'
+      const allApplicationsPerUser = `https://job-tracker-apps-service.herokuapp.com/applications/user/${userId}`
 
-      //get all applications -> wrangles data into useable format
-      return Promise.all([$http.get(allUsersURL), $http.get(allApplicationsURL)])
-        .then((response) => response)
+
+      return Promise.all([$http.get(allUsersURL), $http.get(allApplicationsURL), $http.get(allApplicationsPerUser)])
+        .then((response) => {
+          const userApplications = response[2].data
+          const averageAppsAllUsers = response[1].data.length / response[0].data.length
+
+          return {averageAppsAllUsers, userApplications}
+        })
         .catch((err) => console.error(err))
     }
 
