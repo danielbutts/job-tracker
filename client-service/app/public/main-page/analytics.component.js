3933e9d6a5dcb63f3bdc8c27c583172dad4ccd13
Mike <mahathaway93@gmail.com>
2017-06-08 12:01:29 -0700

    fixed chart loading issue

--- client-service/app/public/main-page/analytics.component.js
+++ client-service/app/public/main-page/analytics.component.js
@@ -5,23 +5,24 @@
       controller: analyticsController,
     })
 
-  analyticsController.$inject = ['analayticsService', '$cookies']
-  function analyticsController(analayticsService, $cookies){
+  analyticsController.$inject = ['analayticsService', '$scope']
+  function analyticsController(analayticsService, $scope){
     const vm = this
 
     vm.$onInit = function(){
+      renderChart()
+    }
 
+    function renderChart(){
       analayticsService.getApplicationData()
         .then((response) => {
           console.log('data from analytics service!',response)
-
-          // vm.series ['Average', 'SingleUser']
-          // vm.labels = ['All Time']
-          // vm.data = [[response.averageAppsAllUsers],[response.userApplications.length]]
+          const userApps = response.userApplications
 
           vm.labels = ["January", "February", "March", "April", "May", "June", "July"]
           vm.data = [[65, 59, 80, 81, 56, 55, 40], [28, 48, 40, 19, 86, 27, 90]]
           vm.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }]
+          console.log(vm)
           vm.options = {
             scales: {
               yAxes: [
@@ -40,11 +41,28 @@
               ]
             }
           }
-          //vm.date = Object.keys(response[1]).filter(application => application.)
+
+          // vm.series ['Average', 'SingleUser']
+          // vm.labels = ['All Time']
+
+
+            function genAverage(average, length){
+              const averageArray = []
+              let i = 0
+              while(i++ < length){
+                averageArray.push(average)
+              }
+              return averageArray
+            }
+
+          // vm.data = [
+          //   genAverage(response.averageAppsAllUsers, userApps.length),
+          //   [userApps.length]
+          // ]
+          $scope.$digest()
         })
         .catch((err) => console.error(err))
     }
-
 
   }
 })()
